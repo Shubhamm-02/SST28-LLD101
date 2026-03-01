ab45b48fc773e47c7d831bd4df797b9b9dad0630
Shubhamm-02 <your-email@example.com>
2026-03-01 16:05:47 +0530

    Done ex8

--- SOLID/Ex8/src/ClubConsole.java
+++ SOLID/Ex8/src/ClubConsole.java
@@ -8,9 +8,9 @@ public class ClubConsole {
     }
 
     public void run() {
-        ClubAdminTools treasurer = new TreasurerTool(ledger);
-        ClubAdminTools secretary = new SecretaryTool(minutes);
-        ClubAdminTools lead = new EventLeadTool(events);
+        FinanceAdmin treasurer = new TreasurerTool(ledger);
+        MinutesAdmin secretary = new SecretaryTool(minutes);
+        EventLead lead = new EventLeadTool(events);
 
         treasurer.addIncome(5000, "sponsor");
         secretary.addMinutes("Meeting at 5pm");
